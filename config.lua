b0b293df5df1324117bc4405ebfed6717067ad24
Hasan <68519154+hsnnnnn@users.noreply.github.com>
2020-07-22 08:22:04 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.CopCount = 0 --whatever you want
+
+Config.Standalone = 1 --xmerfs burayı elleme kanki
+
+Config.Money = 800
+
+Config.npc = {
+        { coords = vector3(1275.72, -1710.29, 53.80), heading = 114.26}
+    }
---
+++ sql-weight.sql
@@ -0,0 +1,3 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+('usefulusb', 'USB', 1, 0, 1),
+('documents', 'Dosyalar', 1, 0, 1),
